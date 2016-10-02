49fe08c82f94216ec5c2bdd07f7c7d32afbc5cb3
llama-0 <AnRozan93@mail.ru>
2016-10-02 23:56:41 +0300

    dummy profile input

--- app/src/main/java/com/example/user/testick/AccountActivity.java
+++ app/src/main/java/com/example/user/testick/AccountActivity.java
@@ -3,6 +3,8 @@ package com.example.user.testick;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -33,6 +35,25 @@ public class AccountActivity extends AppCompatActivity {
                 startActivity(new Intent(AccountActivity.this, LoginActivity.class));
             }
         });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.account_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_add) {
+
+            startActivity(new Intent(AccountActivity.this, AddProfileActivity.class));
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/user/testick/AddProfileActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.testick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_profile);
+    }
+}
